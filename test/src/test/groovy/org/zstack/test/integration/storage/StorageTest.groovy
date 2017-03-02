ff8b2729f9e039989a146da217e7b84b7dce8525
lining <2313806311@qq.com>
2017-03-03 00:14:03 +0800

    fix bug LocalStorageBase.returnStorageCapacityToHost

--- test/src/test/groovy/org/zstack/test/integration/storage/StorageTest.groovy
+++ test/src/test/groovy/org/zstack/test/integration/storage/StorageTest.groovy
@@ -1,5 +1,6 @@
 package org.zstack.test.integration.storage
 
+import org.zstack.test.integration.storage.primary.local.LocalStorageHostRefVOCase
 import org.zstack.test.integration.storage.primary.local.LocalStorageMigrateVolumeCase
 import org.zstack.test.integration.storage.primary.nfs.NfsGCCase
 import org.zstack.test.integration.storage.primary.smp.SMPCapacityCase
@@ -36,7 +37,8 @@ class StorageTest extends Test {
         runSubCases([
                 new LocalStorageMigrateVolumeCase(),
                 new SMPCapacityCase(),
-                new NfsGCCase()
+                new NfsGCCase(),
+                new LocalStorageHostRefVOCase(),
         ])
     }
 }
