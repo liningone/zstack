c3d7cbb28294ba43b060e8b99ac9951de34c6e90
lining <2313806311@qq.com>
2017-04-25 19:25:03 +0800

    test

---
+++ test/src/test/java/org/zstack/test/storage/primary/nfs/NfsCreateVmByIsoCapacityCase.groovy
@@ -0,0 +1,140 @@
+package org.zstack.test.storage.primary.nfs
+
+import org.springframework.http.HttpEntity
+import org.zstack.sdk.ClusterInventory
+import org.zstack.sdk.DiskOfferingInventory
+import org.zstack.sdk.GetPrimaryStorageCapacityResult
+import org.zstack.sdk.ImageInventory
+import org.zstack.sdk.InstanceOfferingInventory
+import org.zstack.sdk.L3NetworkInventory
+import org.zstack.sdk.PrimaryStorageInventory
+import org.zstack.test.integration.storage.Env
+import org.zstack.test.integration.storage.StorageTest
+import org.zstack.testlib.EnvSpec
+import org.zstack.testlib.LocalStorageSpec
+import org.zstack.testlib.PrimaryStorageSpec
+import org.zstack.testlib.SubCase
+import org.zstack.testlib.NfsPrimaryStorageSpec
+import org.zstack.utils.gson.JSONObjectUtil
+import org.zstack.storage.primary.nfs.NfsPrimaryToSftpBackupKVMBackend
+import org.zstack.storage.primary.nfs.NfsPrimaryStorageKVMBackendCommands
+import org.zstack.storage.backup.sftp.SftpBackupStorageConstant
+import org.zstack.header.image.ImageConstant
+//import org.zstack.header.storage.backup.BackupStorageInventory
+import org.zstack.storage.backup.sftp.SftpBackupStorageCommands
+import org.zstack.testlib.BackupStorageSpec
+//import org.zstack.storage.backup.sftp.SftpBackupStorageInventory
+import org.zstack.storage.primary.nfs.NfsPrimaryStorageKVMBackend
+import org.zstack.testlib.NfsPrimaryStorageSpec
+
+
+/**
+ * Created by SyZhao on 2017/4/17.
+ */
+class NfsCreateVmByIsoCapacityCase extends SubCase {
+    EnvSpec env
+
+    @Override
+    void clean() {
+        env.delete()
+    }
+
+    @Override
+    void setup() {
+        useSpring(StorageTest.springSpec)
+    }
+
+    @Override
+    void environment() {
+        env = Env.nfsOneVmEnv()
+    }
+
+    @Override
+    void test() {
+        env.create {
+            testCreateVmByIsoCheckCapacity()
+        }
+    }
+
+    void testCreateVmByIsoCheckCapacity() {
+        PrimaryStorageInventory ps = env.inventoryByName("nfs")
+        ClusterInventory cluster = env.inventoryByName("cluster")
+        ImageInventory image = env.inventoryByName("test-iso")
+        DiskOfferingInventory diskOffering = env.inventoryByName("diskOffering")
+        InstanceOfferingInventory instanceOffering = env.inventoryByName("instanceOffering")
+        L3NetworkInventory l3 = env.inventoryByName("l3")
+        def bs = env.inventoryByName("sftp")
+        //def image_virtual_size = 10737418240 //10G
+        //def image_physical_size = 1073741824 //1G
+        def image_virtual_size = 10 //10G
+        def image_physical_size = 1//1G
+
+
+        //env.simulator(NfsPrimaryStorageKVMBackend.REMOUNT_PATH) { HttpEntity<String> e, EnvSpec espec ->
+        //    def cmd = JSONObjectUtil.toObject(e.getBody(), NfsPrimaryStorageKVMBackendCommands.RemountCmd.class)
+        //    def rsp = new NfsPrimaryStorageKVMBackendCommands.NfsPrimaryStorageAgentResponse()
+        //    rsp.totalCapacity = capacityResult.totalPhysicalCapacity
+        //    rsp.availableCapacity = capacityResult.availablePhysicalCapacity - image_physical_size
+        //    return rsp
+        //}
+        reconnectPrimaryStorage {
+            uuid = ps.uuid
+        }
+
+
+        def download_image_path_invoked = false
+        env.simulator(SftpBackupStorageConstant.DOWNLOAD_IMAGE_PATH) { HttpEntity<String> e, EnvSpec spec ->
+            def cmd = JSONObjectUtil.toObject(e.getBody(), SftpBackupStorageCommands.DownloadCmd.class)
+            BackupStorageSpec bsSpec = spec.specByUuid(cmd.uuid)
+
+            def rsp = new SftpBackupStorageCommands.DownloadResponse()
+            rsp.size = image_physical_size
+            rsp.actualSize = image_virtual_size
+            rsp.availableCapacity = bsSpec.availableCapacity
+            rsp.totalCapacity = bsSpec.totalCapacity
+            download_image_path_invoked = true
+            return rsp
+        }
+
+        ImageInventory sizedImage = addImage {
+            name = "sized-image"
+            url = "http://my-site/foo.iso"
+            backupStorageUuids = [bs.uuid]
+            format = ImageConstant.ISO_FORMAT_STRING
+        }
+
+        assert download_image_path_invoked
+
+        GetPrimaryStorageCapacityResult beforeCapacityResult = getPrimaryStorageCapacity {
+            primaryStorageUuids = [ps.uuid]
+        }
+
+        createVmInstance {
+            name = "crt-vm"
+            instanceOfferingUuid = instanceOffering.uuid
+            imageUuid = sizedImage.uuid
+            l3NetworkUuids = [l3.uuid]
+            rootDiskOfferingUuid = diskOffering.uuid
+        }
+
+        GetPrimaryStorageCapacityResult capacityResult = getPrimaryStorageCapacity {
+            primaryStorageUuids = [ps.uuid]
+        }
+        assert beforeCapacityResult.availableCapacity == capacityResult.availableCapacity + image_virtual_size + image_physical_size
+
+        env.simulator(NfsPrimaryStorageKVMBackend.REMOUNT_PATH) { HttpEntity<String> e, EnvSpec espec ->
+            def cmd = JSONObjectUtil.toObject(e.getBody(), NfsPrimaryStorageKVMBackendCommands.RemountCmd.class)
+            def rsp = new NfsPrimaryStorageKVMBackendCommands.NfsPrimaryStorageAgentResponse()
+            rsp.totalCapacity = capacityResult.totalPhysicalCapacity
+            rsp.availableCapacity = capacityResult.availablePhysicalCapacity - image_physical_size
+            return rsp
+        }
+        reconnectPrimaryStorage {
+            uuid = ps.uuid
+        }
+        GetPrimaryStorageCapacityResult afterCapacityResult = getPrimaryStorageCapacity {
+            primaryStorageUuids = [ps.uuid]
+        }
+        assert beforeCapacityResult.availablePhysicalCapacity == afterCapacityResult.availablePhysicalCapacity + image_physical_size
+    }
+}
