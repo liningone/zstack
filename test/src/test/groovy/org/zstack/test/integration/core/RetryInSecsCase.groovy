9f9f184643f20381138e7f99b992e09bbd88b4ee
lining <2313806311@qq.com>
2017-05-14 11:54:42 +0800

    test

---
+++ test/src/test/groovy/org/zstack/test/integration/core/RetryInSecsCase.groovy
@@ -0,0 +1,26 @@
+package org.zstack.test.integration.core
+
+import org.zstack.testlib.SubCase
+
+/**
+ * Created by lining on 2017/4/10.
+ */
+class RetryInSecsCase extends SubCase {
+
+    @Override
+    void clean() {
+    }
+
+    @Override
+    void setup() {
+    }
+
+    @Override
+    void environment() {
+    }
+
+    @Override
+    void test() {
+
+    }
+}
